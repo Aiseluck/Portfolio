7a2f0eb183230fe7abbc030e5edaaaa1152babd8
aise <aiseluck@gmail.com>
2023-04-04 15:28:44 +0100

    Finished Porfolio for Landscape

--- component/myworks/techwriting.js
+++ component/myworks/techwriting.js
@@ -1,8 +1,39 @@
 import writing from "./techwriting.module.css";
 import close from "@/public/multiply.svg";
 import Image from "next/image";
+import medium from "@/public/medium.svg";
 
 export default function Writing({ view, setView }) {
+  const data = [
+    {
+      title:
+        "Understanding Ethereum’s RLP Serialization: A Technical Exploration on Ethereum Data Encoding",
+      content: "In the world of all underlyin tech",
+      link: "https://medium.com/@Aiseluck/understanding-ethereums-rlp-serialization-a-technical-exploration-on-ethereum-data-encoding-a1fd54b8f58",
+    },
+    {
+      title:
+        "Building a Multi-Sig Wallet Dapp with Solidity, Hardhat, ReactJS, EthersJs, and Wagmi: A Step-by-Step Guide",
+      content: "In the world of all underlyin tech",
+      link: "https://medium.com/@Aiseluck/building-a-multi-sig-wallet-dapp-with-solidity-hardhat-reactjs-ethersjs-and-wagmi-a-9eca60d105cc",
+    },
+    {
+      title:
+        "How to use the Presidential Staking Platform Launched by Avraq.com",
+      content: "In the world of all underlyin tech",
+      link: "https://medium.com/@Aiseluck/how-to-use-the-presidential-staking-platform-launched-by-avraq-com-f8cf10f68bb0",
+    },
+    {
+      title: "What’s “smart” about Smart Contract?",
+      content: "In the world of all underlyin tech",
+      link: "https://medium.com/@Aiseluck/whats-smart-about-smart-contract-2f225c9289e0",
+    },
+    {
+      title: "JavaScript Async/Await is not Just a Syntactical Sugar!!!!",
+      content: "In the world of all underlyin tech",
+      link: "https://medium.com/@Aiseluck/javascript-async-await-is-not-just-a-syntactical-sugar-af54d63dfad8",
+    },
+  ];
   return (
     <div className={writing.main} id={view != "writing" ? writing.hidden : ""}>
       <div
@@ -13,6 +44,25 @@ export default function Writing({ view, setView }) {
       >
         <span>Close Technical Page</span>
         <Image src={close} alt="Close Sign" />
+      </div>
+      <div className={writing.heading}>Pubished Technical Articles</div>
+      <div className={writing.cards}>
+        {data.map((story) => {
+          return (
+            <div className={writing.card} key={story.title}>
+              <h1>{story.title}</h1>
+              <a
+                href={story.link}
+                className={writing.link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div>Check Article on Medium </div>
+                <Image src={medium} alt="medium logo" width={19} />
+              </a>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- component/myworks/web2.js
+++ component/myworks/web2.js
@@ -1,6 +1,10 @@
 import web from "./web2.module.css";
 import close from "@/public/multiply.svg";
 import Image from "next/image";
+import avraq from "@/public/port1.png";
+import www from "@/public/globe.svg";
+import oil from "@/public/port2.png";
+import git from "@/public/socials/github.svg";
 
 export default function Web2({ view, setView }) {
   return (
@@ -13,6 +17,77 @@ export default function Web2({ view, setView }) {
       >
         <span>Close Web2 Page</span>
         <Image src={close} alt="Close Sign" />
+      </div>
+      <div className={web.heading}>Live Web2 Projects</div>
+      <div className={web.cards}>
+        <div className={web.card}>
+          <div className={web.image}>
+            <Image src={avraq} alt="A Landing Page" />
+          </div>
+          <div className={web.content}>
+            <h1>Official Page of Avraq.com</h1>
+            <p>Opening Page of Avraq and it associated website</p>
+            <a href="https://www.avraq.com/" target="_blank" rel="noreferrer">
+              <div className={web.www}>
+                View Live
+                <Image src={www} alt={"globe symbol"} />
+              </div>
+            </a>
+            <div className={web.build}>
+              <p>Built with</p>
+              <ul>
+                <li>Html</li>
+                <li>Css</li>
+                <li>ReactJs</li>
+                <li>NextJs</li>
+                <li>Firebase</li>
+                <li>NodeJs</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className={web.card}>
+          <div className={web.image}>
+            <Image src={oil} alt="A Landing Page" />
+          </div>
+          <div className={web.content}>
+            <h1>Official Page of Xteemwell.com</h1>
+            <p>Opening Page of Xteemwell and it associated website</p>
+            <div className={web.anchor}>
+              <a
+                href="https://xteem.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className={web.www}>
+                  View Live
+                  <Image src={www} alt={"globe symbol"} />
+                </div>
+              </a>
+              <a
+                href="https://github.com/Aiseluck/Xteem"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className={web.www}>
+                  View Source Code
+                  <Image src={git} alt={"globe symbol"} />
+                </div>
+              </a>
+            </div>
+            <div className={web.build}>
+              <p>Built with</p>
+              <ul>
+                <li>Html</li>
+                <li>Css</li>
+                <li>ReactJs</li>
+                <li>NextJs</li>
+                <li>NodeJs</li>
+                <li>Xpress</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
